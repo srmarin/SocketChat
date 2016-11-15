87581e5ffafb9494cb56207ea9cf507807e652f1
Sergio <sergio_basket93@hotmail.com>
2016-11-15 15:39:02 +0100

    Message Observer

---
+++ MensajesChat.java
@@ -0,0 +1,18 @@
+package servidorchat;
+import java.util.Observable;
+
+public class MensajesChat extends Observable{
+  private String mensaje;
+
+  public MensajesChat(){}
+
+  public String getMensaje(){
+    return this.mensaje;
+  }
+
+  public void setMensaje(String mensaje){
+    this.mensaje = mensaje;
+    this.setChanged();
+    this.notifyObservers(this.getMensaje());
+  }
+}
